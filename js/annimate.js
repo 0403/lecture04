4f3a8a4b8601c74181362ddb14dc22c07bfac264
0403 <sfcyokohama@yahoo.co.jp>
2014-05-05 19:12:03 +0900

    課題　テスト

--- js/annimate.js
+++ js/annimate.js
@@ -1,25 +1,40 @@
 var endFlipFoxkeh = function(){
+	/aaa/
 	var foxkeh = document.querySelector("#slidein-foxkeh img");
+	/aaa/
 	foxkeh.removeEventListener("animationend", endFlipFoxkeh);
+	/aaa/
 
 	foxkeh.setAttribute("class", "");
+	/aaa/
 };
 
 var endSlideinFoxkehAndStartFlipFoxkeh = function(){
+	/aaa/
 	var foxkeh = document.querySelector("#slidein-foxkeh img");
+	/aaa/
 
 	foxkeh.removeEventListener("animationend", endSlideinFoxkehAndStartFlipFoxkeh);
+	/aaa/
 	foxkeh.addEventListener("animationend", endFlipFoxkeh);
+	/aaa/
 
 	foxkeh.setAttribute("class", "flip");
+	/aaa/
 };
 
 var startSlideinFoxkeh = function(){
+	/aaa/
 	var foxkeh = document.querySelector("#slidein-foxkeh img");
+	/aaa/
 	foxkeh.setAttribute("class", "slidein");
+	/aaa/
 
 	foxkeh.addEventListener("animationend", endSlideinFoxkehAndStartFlipFoxkeh);
+	/aaa/
 };
 
 var slideinFoxkehButton = document.querySelector("#slidein-foxkeh button");
+/aaa/
 slideinFoxkehButton.addEventListener("click", startSlideinFoxkeh);
+/aaa/
